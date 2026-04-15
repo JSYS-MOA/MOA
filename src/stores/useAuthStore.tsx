59d9b6e43c2337a4c901a26f63d7ea3557ff4f29
kimseojin <wjhy0112@gmail.com>
2026-04-15 13:21:54 +0900

    로그인구현

--- src/stores/useAuthStore.tsx
+++ src/stores/useAuthStore.tsx
@@ -3,7 +3,7 @@ import "../types/auth.ts";
 import type {User} from "../types/auth.ts";
 
 // zustand 스토어 안에 뭐가 들어있는지 미리 정의해두는 것
-type AuthState = {
+interface AuthState {
     user: User | null;
     login: (userData: User) => void;
     logout: () => void;
@@ -14,18 +14,3 @@ export const useAuthStore = create<AuthState>((set) => ({
     login: (userData) => set({ user: userData }),
     logout: () => set({ user: null })
 }));
-//
-// export const useAuthStore = create<any>()(
-//   persist(
-//     (set) => ({
-//       user : null,
-//       login  : (userData:any) => set({ user : userData}),
-//       logout : () => set( {user : null} )
-//     }),
-//
-//     {
-//       name: "auth-storage",
-//     }
-//   )
-// );
-
--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,4 +1,4 @@
-export type User = {
+export interface User {
     employeeId: string;
     departmentId: number;
     roleId: number;
