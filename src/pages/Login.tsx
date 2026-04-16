69f19f0a0f68e3c8551725f9ffbe7cdc8165a717
kimseojin <wjhy0112@gmail.com>
2026-04-16 14:19:59 +0900

    로그인구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,13 @@
 //자주 조회하는 데이터 -> react-query -> 캘린더 등
 //한번만 요청하는 것 -> async/await  -> 로그인
 
-import React,{useState} from "react";
+import {useState} from "react";
 import {useAuthStore} from "../stores/useAuthStore.tsx";
 import {Link, useNavigate} from "react-router";
 import {loginApi} from "../apis/LoginService.tsx";
 import { FiAlertCircle } from "react-icons/fi";
 import "../assets/styles/login.css";
+import type { SyntheticEvent } from "react";
 
 const Login = () =>{
 
@@ -19,7 +20,7 @@ const Login = () =>{
   const {login} = useAuthStore();
   const navigate = useNavigate();
 
-  const handleSubmit = async (e: React.SyntheticEvent) => {
+  const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault(); //폼의 기본 동작 = 페이지 새로고침 => handleSubmit 안에 코드 실행 안됨 >이걸 막기위해
 
     setEmployeeIdError("");
@@ -58,29 +59,33 @@ const Login = () =>{
   return(
     <div className="login-wrap">
       <div className="logo">
-        <h2>MOA</h2>
+        <h1>MOA</h1>
         <p>All-in-One 비즈니스 솔루션</p>
       </div>
       <form onSubmit={handleSubmit}>
-        <h4 className="title">사원코드</h4>
-        <input
-            value={employeeId}
-            onChange={(e) => setEmployeeId(e.target.value)}
-            placeholder="사원코드 입력"
-        />
+        <div className="login-id">
+          <p className="title">사원코드</p>
+          <input
+              value={employeeId}
+              onChange={(e) => setEmployeeId(e.target.value)}
+              placeholder="사원코드 입력"
+          />
+        </div>
         {employeeIdError &&
             <p className="error-icon">
               <span><FiAlertCircle /></span>
               {employeeIdError}
             </p>
         }
-        <h4 className="title">비밀번호</h4>
-        <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="비밀번호 입력"
-        />
+        <div className="login-password">
+          <p className="title">비밀번호</p>
+          <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="비밀번호 입력"
+          />
+        </div>
         {passwordError &&
             <p className="error-icon">
               <span><FiAlertCircle /></span>
@@ -89,6 +94,7 @@ const Login = () =>{
         }
         {loginError &&
             <div className="login-error">
+              <span><FiAlertCircle /></span>
               {loginError}
             </div>
         }
@@ -102,9 +108,9 @@ const Login = () =>{
         </button>
         <p className="privacy-agree">
           서비스 이용 시 <span>이용약관</span>과
-          <span>개인정보처리방침</span>에 동의한 것으로 간주합니다
+          <span> 개인정보처리방침</span>에 동의한 것으로 간주합니다
         </p>
-        <Link to="/find-password">비밀번호찾기</Link>
+        <Link to="/find-password" className="login-findPassword">비밀번호찾기</Link>
       </form>
     </div>
   )
