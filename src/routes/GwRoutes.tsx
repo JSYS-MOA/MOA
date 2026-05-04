2c9bf3f6247c2dfb02400d4793e4f89a833a9b9c
박준 <157086374+mutajune@users.noreply.github.com>
2026-05-04 18:08:10 +0900

    Update GwRoutes.tsx

--- src/routes/GwRoutes.tsx
+++ src/routes/GwRoutes.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import Approvals from '../pages/approvals/Approvals';
 import TeamMembers from '../pages/approvals/TeamMembers';
 import ApprovalsWait from '../pages/approvals/ApprovalsWait';
+import Base from '../pages/base/Base';
 
  const GwRouter = () => {
   return (
     <Routes>
       <Route index element={<Approvals/>} />
       <Route path="approvals" element={<Approvals/>} />
+      <Route path="approvalForms" element={<Base apiType="form" />} />
       <Route path='teamMembers' element={<TeamMembers/>} />
       <Route path='approvalWait' element={<ApprovalsWait/>}/>
     </Routes>
