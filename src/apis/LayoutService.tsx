0a4ce1dd04c9451296ece6f8a38bb20a0a61cf63
박준 <157086374+mutajune@users.noreply.github.com>
2026-05-04 15:14:58 +0900

    Update LayoutService.tsx

--- src/apis/LayoutService.tsx
+++ src/apis/LayoutService.tsx
@@ -1,12 +1,23 @@
 import axios from "axios";
+import {useAuthStore} from "../stores/useAuthStore.tsx";
 
 // const API = "http://localhost/api";
 const API = "https://moa-server.onrender.com/api";
 
 export async function layoutApi(){
+ 
+    const { user } = useAuthStore.getState(); 
+    const employeeId = user?.employeeId;
+
+    if (!employeeId) {
+        console.error("아이디가 없습니다!");
+        return;
+    }
+
     const { data } = await axios.get(
         `${ API }/auth/layout`,
-        { withCredentials: true } //서버와 주소가 달라서 브라우저가 JSESSIONID 쿠키를 같이 보내야 유저 인식(필수)
+        
+        {  params: { employeeId: employeeId } , withCredentials: true } //서버와 주소가 달라서 브라우저가 JSESSIONID 쿠키를 같이 보내야 유저 인식(필수)
     );
     return data;
 }
